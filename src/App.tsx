5599d25c508091d0d40635d90c1b83dc1f4f3031
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-05-28 22:26:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export default function App() {
     ScrollTrigger.create({
       onEnter: () =>
         a.play()
-      onBack: () =>
+      onLeave: () =>
         a.reverse()
     });
   }, []);
