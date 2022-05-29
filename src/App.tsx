896279c88489a5d1f78d2a6a4672ab1547d7b773
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-05-28 22:19:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ export default function App() {
       autoAlpha: 1
     }, 'start');
 
-    const a = gsap.to(test.current, { paused: true, x: 100, duration: 2, ease: "bounce", delay: 1, scrollTrigger: { trigger:test.current, scrub:true } });
+    const a = gsap.to(test.current, { paused: true, x: 100, duration: 2, delay: 1, scrollTrigger: { trigger:test.current, scrub:true } });
 
     ScrollTrigger.create({
       onEnter: () =>
