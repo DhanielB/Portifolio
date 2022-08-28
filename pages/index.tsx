4ca917507cb863f4c6def5f2ff6b2af3e4665e45
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-08-27 22:57:05 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function App() {
     const menuAnimation = gsap.from(menuRef.current, { 
       yPercent: -100,
       paused: true,
-      duration: 0.2
+      duration: 0
     }).progress(1).reverse();
 
     ScrollTrigger.create({
